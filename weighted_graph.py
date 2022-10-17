7b26e1f206f263bcfc364b480ab739c81adbe955
Mateus-Henr <mateush.study@gmail.com>
2022-10-17 17:49:37 -0300

    Create function documentations

--- weighted_graph.py
+++ weighted_graph.py
@@ -1,22 +1,133 @@
 class Edge:
-    # Constructor, Time O(1) Space O(1)
+    """
+    A class used to represent an edge.
+
+    ...
+
+    Attributes
+    ----------
+    connected_vertex : int
+        an integer that represents a vertex.
+    weight : float
+        the weight of the edge.
+
+    Methods
+    -------
+     __str__(self)
+        Returns a string representation of the edge.
+    """
+
+
     def __init__(self, v, w):
+        """
+        Parameters
+        ----------
+        v : int
+            an integer that represents a vertex.
+        w : float
+            the weight of the edge.
+        """
+
         self.connected_vertex = v
         self.weight = w
 
-    # Time O(1) Space O(1)
+
     def __str__(self):
+        """
+        Returns a string representation of the edge.
+        """
+
         return "(" + str(self.connected_vertex) + "," + str(self.weight) + ")"
 
 
 class GraphWeighted:
-    # Constructor, Time O(1) Space O(1)
+    """
+    A class used to represent an undirected graph backed by an adjacency list.
+
+    ...
+
+    Attributes
+    ----------
+    v_number : int
+        an integer that represents the number of vertices.
+    adj : dict
+        a dictionary that represents the adjacency list of the graph.
+
+    Methods
+    -------
+    add_edge(self, a, b, w)
+        Adds a new edge to the graph.
+
+    get_order(self)
+        Returns the order of the graph.
+
+    size(self)
+        Returns the size of the graph.
+
+    get_neighbours(self, v)
+        Returns the neighbours of a vertex.
+
+    degree_of_vertex(self, v)
+        Returns the degree of a vertex.
+
+    degree_sequence(self)
+        Returns the degree sequence of the graph.
+
+    eccentricity(self, v)
+        Returns the eccentricity of a vertex.
+
+    bellman_ford(self, vertex)
+        Returns the distances of all vertices from a vertex using bellman ford algorithm.
+
+    radius(self)
+        Returns the radius of the graph.
+
+    diameter(self)
+        Returns the diameter of the graph.
+
+    center(self)
+        Returns the center of the graph.
+
+    dfs(self, v)
+        Returns the dfs of the graph.
+
+    dfs_util(self, v, visited)
+        Helper function for dfs.
+
+    dfs_not_visited(self)
+        Returns vertices that were not visited by the dfs.
+
+    closeness_centrality(self, v)
+        Returns the closeness centrality of a vertex.
+    """
+
+
     def __init__(self, v_number):
+        """
+        Parameters
+        ----------
+        v_number : int
+            an integer that represents the number of vertices.
+        """
+
         self.v_number = v_number
         self.adj = {}
 
-    # Add edges including adding nodes, Time O(1) Space O(1)
+
     def add_edge(self, a, b, w):
+        """
+        Adds a new edge to the graph.
+
+        Parameters
+        ----------
+        a : int
+            first vertex.
+        b : int
+            second vertex.
+        w : float
+            weight of the edge.
+        """
+
         if a not in self.adj:
             self.adj[a] = []
         if b not in self.adj:
@@ -26,40 +137,97 @@ class GraphWeighted:
         edge2 = Edge(a, w)
         self.adj[b].append(edge2)
 
-    # Get order of a weighted non-directional graph
-    # Time O(1), Space O(1)
+
     def get_order(self):
+        """
+        Returns the order of the graph.
+
+        Returns
+        -------
+        order of the graph.
+        """
+
         return self.v_number
 
-    # Size of a weighted non-directional graph
-    # Time O(V+E), Space O(V)
+
     def size(self):
+        """
+        Returns the size of the graph.
+
+        Returns
+        -------
+        size of the graph.
+        """
+
         return len(self.adj)
 
-    # Neighbours of a weighted non-directional graph
-    # Time O(1), Space O(1)
+
     def get_neighbours(self, v):
+        """
+        Returns the neighbours of a vertex.
+
+        Parameters
+        ----------
+        v : int
+            a vertex.
+
+        Returns
+        -------
+        list of neighbours.
+        """
+
         edges = []
         for edge in self.adj[v]:
             edges.append(edge.connected_vertex)
         return edges
 
-    # Degree of a vertex of a weighted non-directional graph
-    # Time O(1), Space O(1)
+
     def degree_of_vertex(self, v):
+        """
+        Returns the degree of a vertex.
+
+        Parameters
+        ----------
+        v : int
+            a vertex.
+
+        Returns
+        -------
+        degree of the vertex.
+        """
+
         return len(self.adj[v])
 
-    # Degree sequence of a weighted non-directional graph
-    # Time O(V+E), Space O(V)
+
     def degree_sequence(self):
+        """
+        Returns the degree sequence of the graph.
+
+        Returns
+        -------
+        list of degrees.
+        """
+
         degree = []
         for k, v in self.adj.items():
             degree.append(len(v))
         return degree
 
-    # Eccentricity of a vertex of a weighted undirected graph using bellman ford algorithm
-    # Time O(V*E), Space O(V)
+
     def eccentricity(self, v):
+        """
+        Returns the eccentricity of a vertex using bellman ford algorithm.
+
+        Parameters
+        ----------
+        v : int
+            a vertex.
+
+        Returns
+        -------
+        eccentricity of the vertex.
+        """
+
         dist = self.bellman_ford(v)
         max = 0
         for k, v in dist.items():
@@ -67,9 +235,21 @@ class GraphWeighted:
                 max = v
         return max
 
-    # Bellman ford algorithm of a weighted undirected graph that detects negative cycles
-    # Time O(V*E), Space O(V)
+
     def bellman_ford(self, vertex):
+        """
+        Returns the distances of all vertices from a vertex using bellman ford algorithm.
+
+        Parameters
+        ----------
+        vertex : int
+            a vertex.
+
+        Returns
+        -------
+        dictionary of distances.
+        """
+
         dist = {}
         for k, v in self.adj.items():
             dist[k] = self.v_number
@@ -85,9 +265,16 @@ class GraphWeighted:
                     return dict()
         return dist
 
-    # Radius of a weighted non-directional graph
-    # Time O(V*V*E), Space O(V)
+
     def radius(self):
+        """
+        Returns the radius of the graph.
+
+        Returns
+        -------
+        radius of the graph.
+        """
+
         min = self.v_number
         for k, v in self.adj.items():
             e = self.eccentricity(k)
@@ -95,9 +282,16 @@ class GraphWeighted:
                 min = e
         return min
 
-    # Diameter of a weighted non-directional graph
-    # Time O(V*V*E), Space O(V)
+
     def diameter(self):
+        """
+        Returns the diameter of the graph.
+
+        Returns
+        -------
+        diameter of the graph.
+        """
+
         max = 0
         for k, v in self.adj.items():
             e = self.eccentricity(k)
@@ -105,9 +299,16 @@ class GraphWeighted:
                 max = e
         return max
 
-    # Center of a weighted non-directional graph
-    # Time O(V*V*E), Space O(V)
+
     def center(self):
+        """
+        Returns the center of the graph.
+
+        Returns
+        -------
+        center of the graph.
+        """
+
         ecc = {}
         for k, v in self.adj.items():
             ecc[k] = self.eccentricity(k)
@@ -118,26 +319,61 @@ class GraphWeighted:
                 min_index = k
         return min_index
 
-    # Get sequence of vertex of dfs traversal
-    # Time O(V+E), Space O(V)
+
     def dfs(self, v):
+        """
+        Returns the dfs of the graph.
+
+        Parameters
+        ----------
+        v : int
+            a vertex.
+
+        Returns
+        -------
+        list of vertices in dfs order.
+        """
+
         visited = {}
         sequence = []
         self.dfs_util(v, visited, sequence)
         return sequence
 
-    # Util function for dfs
-    # Time O(V+E), Space O(V)
+
     def dfs_util(self, v, visited, sequence):
+        """
+        Helper function for dfs.
+
+        Parameters
+        ----------
+        v : int
+            a vertex.
+        visited : dict
+            dictionary of visited vertices.
+        sequence : list
+            list of vertices in dfs order.
+
+        Returns
+        -------
+        list of vertices in dfs order.
+        """
+
         visited[v] = True
         sequence.append(v)
         for edge in self.adj[v]:
             if edge.connected_vertex not in visited:
                 self.dfs_util(edge.connected_vertex, visited, sequence)
 
-    # Get sequence of vertex not visited by dfs traversal
-    # Time O(V+E), Space O(V)
+
     def dfs_not_visited(self):
+        """
+        Returns vertices that were not visited by the dfs.
+
+        Returns
+        -------
+        list of vertices that were not visited by the dfs.
+        """
+
         visited = {}
         sequence = []
         for k, v in self.adj.items():
@@ -145,9 +381,21 @@ class GraphWeighted:
                 self.dfs_util(k, visited, sequence)
         return sequence
 
-    # Closeness centrality of a weighted undirected graph
-    # Time O(V*V*E), Space O(V)
+
     def closeness_centrality(self, v):
+        """
+        Returns the closeness centrality of a vertex.
+
+        Parameters
+        ----------
+        v : int
+            a vertex.
+
+        Returns
+        -------
+        closeness centrality of the vertex.
+        """
+
         dist = self.bellman_ford(v)
         sum = 0
         for k, v in dist.items():
@@ -156,15 +404,35 @@ class GraphWeighted:
             return 0
         return (self.v_number - 1) / sum
 
-    # Get weighted non-directional graph
-    # Time O(1), Space O(1)
+
     def get_graph(self):
+        """
+        Returns the graph.
+
+        Returns
+        -------
+        graph.
+        """
+
         return self.adj
 
 
-# Read a json and generate a text file where the first line is the number of vertex and the following lines are the two vertex and the weight without using json library
-# Time O(V+E), Space O(V)
 def generate_text_file_from_json_file(json_file, text_file):
+    """
+    Generates a text file from a json file.
+
+    Parameters
+    ----------
+    json_file : str
+        json file.
+    text_file : str
+        text file.
+
+    Returns
+    -------
+    None.
+    """
+
     with open(json_file, 'r') as f:
         data = f.read()
     with open(text_file, 'w') as f:
@@ -174,9 +442,21 @@ def generate_text_file_from_json_file(json_file, text_file):
                 f.write(data[i - 1] + " " + data[i + 2] + " " + data[i + 8] + "\n")
 
 
-# Generate json file following the JSON format file
-# Time O(V+E), Space O(V)
 def generate_json_file_from_text_file(text_file, json_file):
+    """
+    Generates a json file from a text file.
+
+    Parameters
+    ----------
+    text_file : str
+        text file.
+    json_file : str
+        json file.
+
+    Returns
+    -------
+    None.
+    """
     with open(text_file, 'r') as f:
         data = f.read()
     with open(json_file, 'w') as f:
